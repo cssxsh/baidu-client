f8c6e327c60ef6d6ac961683d1210f783c4b7aec
cssxsh <cssxsh@gmail.com>
2021-11-10 22:03:08 +0800

    fix: NetDiskCreateFile

--- baidu-netdisk/src/main/kotlin/xyz/cssxsh/baidu/disk/NetDiskCreateFile.kt
+++ baidu-netdisk/src/main/kotlin/xyz/cssxsh/baidu/disk/NetDiskCreateFile.kt
@@ -8,24 +8,24 @@ data class NetDiskCreateFile(
     @SerialName("category")
     override val category: CategoryType,
     @SerialName("ctime")
-    override val created: Long,
+    override val created: Long = 0,
     @SerialName("errno")
     val errno: Int,
     @SerialName("fs_id")
-    override val id: Long,
+    override val id: Long = 0,
     @SerialName("isdir")
     @Serializable(with = NumberToBooleanSerializer::class)
-    override val isDir: Boolean,
+    override val isDir: Boolean = false,
     @SerialName("md5")
     override val md5: String = "",
     @SerialName("mtime")
-    override val modified: Long,
+    override val modified: Long = 0,
     @SerialName("name")
-    val name: String,
+    val name: String = "",
     @SerialName("status")
     val status: Int? = null,
     @SerialName("path")
-    override val path: String,
+    override val path: String = "",
     @SerialName("server_filename")
     override val filename: String = "",
     @SerialName("size")
