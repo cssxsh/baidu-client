7e280b51c9aadfbc9e1f234ce6ba06474934dfa9
cssxsh <cssxsh@gmail.com>
2022-02-04 13:57:22 +0800

    build: 2.0.6

--- baidu-aip/build.gradle.kts
+++ baidu-aip/build.gradle.kts
@@ -26,6 +26,18 @@ kotlin {
             languageSettings.optIn("kotlin.RequiresOptIn")
         }
     }
+    target.compilations {
+        all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
