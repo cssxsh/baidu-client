659304b7b315976ab836ee195b3239552030c52e
cssxsh <cssxsh@gmail.com>
2022-08-04 18:03:50 +0800

    fix: appDataFolder

--- baidu-netdisk/src/main/kotlin/xyz/cssxsh/baidu/disk/NetDiskClient.kt
+++ baidu-netdisk/src/main/kotlin/xyz/cssxsh/baidu/disk/NetDiskClient.kt
@@ -6,7 +6,7 @@ import java.nio.file.*
 public interface NetDiskClient : BaiduUserAuthClient<BaiduAuthConfig> {
     public val appDataFolder: Path
 
-    public fun appDataFolder(path: String): Path = appDataFolder.resolve(path)
+    public fun appDataFolder(path: String): String = appDataFolder.resolve(path).joinToString("/")
 
     public companion object {
         public const val TIMEOUT: Long = 30 * 1000L
