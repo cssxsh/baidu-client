da9f1d9c4efe2edde3b12a2e634b55dfc74b6925
cssxsh <cssxsh@gmail.com>
2022-08-04 22:52:29 +0800

    feat: SpeechPerson docs

--- baidu-aip/src/main/kotlin/xyz/cssxsh/baidu/aip/tts/SpeechPerson.kt
+++ baidu-aip/src/main/kotlin/xyz/cssxsh/baidu/aip/tts/SpeechPerson.kt
@@ -1,27 +1,30 @@
 package xyz.cssxsh.baidu.aip.tts
 
+/**
+ * [demo](https://ai.baidu.com/tech/speech/tts_online)
+ */
 public object SpeechPerson {
     /**
      * 基础音库
      */
     public object Base {
         /**
-         * 成熟女声
+         * 度小美-成熟女声
          */
         public const val MatureFemale: Int = 0
 
         /**
-         * 成熟男声
+         * 度小宇-成熟男声
          */
         public const val MatureMale: Int = 1
 
         /**
-         * 磁性男声
+         * 度逍遥-磁性男声
          */
         public const val MagneticMale: Int = 3
 
         /**
-         * 可爱女童
+         * 度丫丫-可爱女童
          */
         public const val LovelyGirl: Int = 4
     }
@@ -31,37 +34,37 @@ public object SpeechPerson {
      */
     public object Boutique {
         /**
-         * 情感女声
+         * 度小娇-情感女声
          */
         public const val EmotionalFemale: Int = 5
 
         /**
-         * 可爱女童
+         * 度米朵-可爱女童
          */
         public const val LovelyGirl: Int = 103
 
         /**
-         * 情感男声
+         * 度博文-情感男声
          */
         public const val EmotionalMale: Int = 106
 
         /**
-         * 可爱女声
+         * 度小萌-可爱女声
          */
         public const val LovelyFemale: Int = 111
 
         /**
-         * 活泼女童
+         * 度小童-活泼女童
          */
         public const val LivelyGirl: Int = 110
 
         /**
-         * 磁性男声
+         * 度逍遥-磁性男声
          */
         public const val MagneticMale: Int = 5003
 
         /**
-         * 甜美女声
+         * 度小鹿-甜美女声
          */
         public const val SweetFemale: Int = 5118
     }
@@ -72,42 +75,42 @@ public object SpeechPerson {
     public object Top {
 
         /**
-         * 磁性男声
+         * 度逍遥-磁性男声
          */
         public const val MagneticMale: Int = 4003
 
         /**
-         * 成熟女声
+         * 度小雯-成熟女声
          */
         public const val MatureFemale: Int = 4100
 
         /**
-         * 成熟女声
+         * 度米朵-可爱女童
          */
         public const val LovelyGirl: Int = 4103
 
         /**
-         * 清澈女声
+         * 度灵儿-清澈女声
          */
         public const val ClearFemale: Int = 4105
 
         /**
-         * 情感男声
+         * 度博文-情感男声
          */
         public const val EmotionalMale: Int = 4106
 
         /**
-         * 成熟男声
+         * 度小贤-成熟男声
          */
         public const val MatureMale: Int = 4115
 
         /**
-         * 情感女声
+         * 度小乔-情感女声
          */
         public const val EmotionalFemale: Int = 4117
 
         /**
-         * 甜美女声
+         * 度小鹿-甜美女声
          */
         public const val SweetFemale: Int = 4119
 
