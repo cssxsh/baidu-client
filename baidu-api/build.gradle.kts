25c1df8e159ece71bd4a11d16fb431f06533503b
cssxsh <cssxsh@gmail.com>
2022-11-05 14:57:21 +0800

    update: ktor 2.1.3

--- baidu-api/build.gradle.kts
+++ baidu-api/build.gradle.kts
@@ -13,10 +13,10 @@ mavenCentralPublish {
 }
 
 dependencies {
-    api("io.ktor:ktor-client-okhttp:2.1.2")
-    api("io.ktor:ktor-client-encoding:2.1.2")
-    api("io.ktor:ktor-client-content-negotiation:2.1.2")
-    api("io.ktor:ktor-serialization-kotlinx-json:2.1.2")
+    api("io.ktor:ktor-client-okhttp:2.1.3")
+    api("io.ktor:ktor-client-encoding:2.1.3")
+    api("io.ktor:ktor-client-content-negotiation:2.1.3")
+    api("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
     api("com.squareup.okhttp3:okhttp:4.10.0")
     testImplementation(kotlin("test"))
 }
