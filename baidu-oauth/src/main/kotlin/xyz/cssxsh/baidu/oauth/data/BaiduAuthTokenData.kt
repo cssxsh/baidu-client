aa392ddd77d6aa38d27686355888b230f4b41eb5
cssxsh <cssxsh@gmail.com>
2023-01-16 17:40:19 +0800

    add: BaiduAuthTokenData Serializable

--- baidu-oauth/src/main/kotlin/xyz/cssxsh/baidu/oauth/data/BaiduAuthTokenData.kt
+++ baidu-oauth/src/main/kotlin/xyz/cssxsh/baidu/oauth/data/BaiduAuthTokenData.kt
@@ -1,14 +1,21 @@
 package xyz.cssxsh.baidu.oauth.data
 
+import kotlinx.serialization.*
 import xyz.cssxsh.baidu.oauth.*
 import java.time.*
 
 /**
  * 百度认证数据
  */
+@Serializable
 public data class BaiduAuthTokenData(
+    @Contextual
+    @SerialName("expires")
     override var expires: OffsetDateTime = OffsetDateTime.MIN,
+    @SerialName("access_token")
     override var accessTokenValue: String = "",
+    @SerialName("refresh_token")
     override var refreshTokenValue: String = "",
+    @SerialName("scope")
     override var scope: List<String> = emptyList()
 ) : BaiduAuthStatus
